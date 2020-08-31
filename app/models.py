2166d1d375f521dacb1b853fc63b86f94896a12f
quebim <kevinledesmam95@gmail.com>
2020-08-31 07:04:09 -0300

    models fix

--- app/models.py
+++ app/models.py
@@ -11,6 +11,13 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=150, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
@@ -21,12 +28,9 @@ class Product(models.Model):
     name = models.CharField(max_length=150, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=150, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=150, null=True)
+    description = models.CharField(max_length=150, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=150, null=True)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
@@ -41,7 +45,6 @@ class Order(models.Model):
 
     cutomer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
-    tags = models.ManyToManyField(Tag)
     status = models.CharField(max_length=150, null=True, choices=STATUS)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
