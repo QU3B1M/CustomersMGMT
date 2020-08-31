e4de550660c723b9b23918b8ed006e12895d6dd6
quebim <kevinledesmam95@gmail.com>
2020-08-31 08:03:36 -0300

    home view and templates updated

--- app/views.py
+++ app/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 def home(request):
-    return render(request, 'app/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+
+    done = orders.filter(status='Done').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {
+        'orders':orders, 
+        'customers':customers, 
+        'total_customers':total_customers,
+        'total_orders':total_orders,
+        'done':done,
+        'pending':pending
+    }
+
+    return render(request, 'app/dashboard.html', context)
 
 def products(request):
-    return render(request, 'app/products.html')
+    products = Product.objects.all()
+    return render(request, 'app/products.html', {'products': products})
 
 def customer(request):
     return render(request, 'app/customer.html')
