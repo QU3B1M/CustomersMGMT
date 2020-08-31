43851c307bdb7f04cee53d40c9e9a1df610c4714
quebim <kevinledesmam95@gmail.com>
2020-08-31 06:55:05 -0300

    Static files added, models created

--- app/models.py
+++ app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=150, null=True)
+    phone = models.CharField(max_length=20, null=True)
+    email = models.EmailField(unique=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+        # ()
+    )
+
+    name = models.CharField(max_length=150, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=150, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=150, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('In Progress', 'In Progress'),
+        ('Done', 'Done'),
+    )
+
+    cutomer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=150, null=True, choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
