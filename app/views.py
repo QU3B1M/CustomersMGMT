428a71ecf675285152e8fe31845b86fcea6295c9
quebim <kevinledesmam95@gmail.com>
2020-09-01 07:54:33 -0300

    customer view and templates updated

--- app/views.py
+++ app/views.py
@@ -28,5 +28,15 @@ def products(request):
     products = Product.objects.all()
     return render(request, 'app/products.html', {'products': products})
 
-def customer(request):
-    return render(request, 'app/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    orders_count = orders.count()
+
+    context = {
+        'customer':customer,
+        'orders':orders,
+        'orders_count':orders_count
+    }
+
+    return render(request, 'app/customer.html', context)
