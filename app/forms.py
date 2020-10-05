8ecbd4aa792beb4866de9c9bcbd02527fe0c4fa8
Kevin Ledesma <kevinledesmam95@gmail.com>
2020-10-05 08:57:53 -0300

    Login and Singup Page finished

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,8 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 from .models import Order, Customer
 
 
@@ -14,3 +18,26 @@ class CustomerForm(ModelForm):
     class Meta:
         model = Customer
         fields = '__all__'
+        
+
+class CreateUserForm(UserCreationForm):
+    username = forms.CharField(label='username',required=True, widget=forms.TextInput(attrs={'placeholder':'Username'}))
+    email = forms.EmailField(label = 'email', required=True, widget=forms.TextInput(attrs={'placeholder':'Email'}))
+    password1 = forms.CharField(label='password',required=True, widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
+    password2 = forms.CharField(label='password',required=True, widget=forms.PasswordInput(attrs={'placeholder':'Password Confirmation'}))
+    
+
+    class Meta:
+        model = User
+        fields = [
+            'username', 
+            'email', 
+            'password1', 
+            'password2'
+        ]
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("A user with tha email already exists")
+        return email
--- app/views.py
+++ app/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
 
 
+@login_required(login_url='login')
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
-
     total_customers = customers.count()
     total_orders = orders.count()
-
     done = orders.filter(status='Done').count()
     pending = orders.filter(status='Pending').count()
 
@@ -28,12 +31,53 @@ def home(request):
     return render(request, 'app/dashboard.html', context)
 
 
+def signup_login(request):
+    if request.user.is_authenticated:
+        return redirect('home')     
+    else:
+        form = CreateUserForm()        
+        if request.method == "POST":  
+             
+            if request.POST.get('submit') == 'sign_up':            
+                form = CreateUserForm(request.POST)   
+                if form.is_valid():
+                    form.save()
+                    user = form.cleaned_data.get('username')
+                    messages.success(request, 'User {} was created successfully'.format(user))
+                else: 
+                    messages.error(request, 'Nope')
+                    
+            elif request.POST.get('submit') == 'sign_in':
+                username = request.POST.get('username')
+                password = request.POST.get('password')
+                user = authenticate(request, username=username, password=password)                    
+                if user is not None:
+                    login(request, user)
+                    return redirect('home')
+                else:
+                    messages.info(request, 'Username or Password is Incorrect')
+                
+        context = {
+            'form': form
+        }
+        
+        return render(request, 'app/login.html', context)
+
+
+@login_required(login_url='login')
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
 def products(request):
     products = Product.objects.all()
     
     return render(request, 'app/products.html', {'products': products})
 
 
+@login_required(login_url='login')
 def customer(request, pk):
     customer = Customer.objects.get(id=pk)
     orders = customer.order_set.all()
@@ -51,6 +95,7 @@ def customer(request, pk):
     return render(request, 'app/customer.html', context)
 
 
+@login_required(login_url='login')
 def create_order(request, pk):
     OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=1, can_delete=False)
     customer = Customer.objects.get(id=pk)
@@ -72,8 +117,8 @@ def create_order(request, pk):
     return render(request, 'app/order_form.html', context)
 
 
-def update_order(request, pk):
-    
+@login_required(login_url='login')
+def update_order(request, pk):    
     order = Order.objects.get(id=pk)
     form = OrderForm(instance=order)
     
@@ -91,8 +136,8 @@ def update_order(request, pk):
     return render(request, 'app/update_order_form.html', context)
 
 
-def delete_order(request, pk):
-    
+@login_required(login_url='login')
+def delete_order(request, pk):    
     order = Order.objects.get(id=pk)
     
     if request.method == 'POST':
@@ -104,5 +149,3 @@ def delete_order(request, pk):
         }
     
     return render(request, 'app/delete.html', context)
-
-
